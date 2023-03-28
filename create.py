97357510cdf2afb14ddbbfd8d097c87edb3b6306
Joe <qahack23@proton.me>
2023-03-28 12:54:30 +0100

    Test

--- create.py
+++ create.py
@@ -1,11 +1,16 @@
 from app import app, db
 from sqlalchemy import create_engine
 from pandas import read_csv
+# from os import getenv
 # from pdb import set_trace
+
+# user = getenv('user')
+# password = getenv('password')
 
 # set_trace()
 
 engine = create_engine('sqlite:///projectdb')
+# f"mysql+pymysql://{user}:{password}@server:3306/database"
 
 with app.app_context():
     db.create_all()
